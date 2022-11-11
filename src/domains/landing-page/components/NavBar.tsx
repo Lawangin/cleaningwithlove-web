f9fe50d59e2c348277b4a7277271d4e899e1e576
Lawangin <lawangin.khan@gmail.com>
2022-11-11 01:45:44 -0500

    changed amplify config scope

--- src/domains/landing-page/components/NavBar.tsx
+++ src/domains/landing-page/components/NavBar.tsx
@@ -42,10 +42,27 @@ const envVars = {
     process.env.NEXT_PUBLIC_AWS_USER_FILES_S3_BUCKET_REGION
 }
 
+const envS3 = {
+  Auth: {
+    identityPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_IDENTITY_POOL_ID, // (required) - Amazon Cognito Identity Pool ID
+    region: process.env.NEXT_PUBLIC_AWS_COGNITO_REGION, // (required) - Amazon Cognito Region
+    userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID, // (optional) - Amazon Cognito User Pool ID
+    userPoolWebClientId: process.env.NEXT_PUBLIC_AWS_USER_POOLS_WEB_CLIENT_ID, // (optional) - Amazon Cognito Web Client ID (App client secret needs to be disabled)
+    aws_mandatory_sign_in: 'disable'
+  },
+  Storage: {
+    AWSS3: {
+      bucket: process.env.NEXT_PUBLIC_AWS_USER_FILES_S3_BUCKET, // (required) -  Amazon S3 bucket name
+      region: process.env.NEXT_PUBLIC_AWS_USER_FILES_S3_BUCKET_REGION,
+      identityPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_IDENTITY_POOL_ID // (optional) -  Amazon service region
+    }
+  }
+}
+
 console.log(envVars)
 logger.debug('awsconfigs', envVars)
 
-Amplify.configure(envVars)
+Amplify.configure(envS3)
 
 const NavBar = (): JSX.Element => {
   const [imgUrl, setImgUrl] = useState<string | null>(null)
