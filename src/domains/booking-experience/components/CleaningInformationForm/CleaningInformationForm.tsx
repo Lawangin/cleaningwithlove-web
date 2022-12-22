8bf50b6ea64ef601c12d39105cb42a99c92a9207
Lawangin <lawangin.khan@gmail.com>
2022-12-22 02:41:15 -0500

    counter functionality added to context and perserves state

--- src/domains/booking-experience/components/CleaningInformationForm/CleaningInformationForm.tsx
+++ src/domains/booking-experience/components/CleaningInformationForm/CleaningInformationForm.tsx
@@ -1,90 +1,90 @@
-import { Formik, Field, Form, FormikHelpers } from "formik";
-import Button from "../../../global/components/Button";
-import Checkbox from "../../../global/components/Checkbox";
-import CustomInputComponent from "../../../global/components/CustomInputComponent";
-import { useBookingContext } from "../../context/BookingContext";
-import FormCounter from "./FormCounter";
-import PriceQuoteLabel from "./PriceQuoteLabel";
+import { Formik, Field, Form, FormikHelpers } from 'formik'
+import Button from '../../../global/components/Button'
+import Checkbox from '../../../global/components/Checkbox'
+import CustomInputComponent from '../../../global/components/CustomInputComponent'
+import { useBookingContext } from '../../context/BookingContext'
+import FormCounter from './FormCounter'
+import PriceQuoteLabel from './PriceQuoteLabel'
 
 interface Values {
-  firstName: string;
-  lastName: string;
-  email: string;
+  bedroom: string
+  lastName: string
+  email: string
 }
 
 const CleaningInformationForm = (): JSX.Element => {
-  const { setPageName } = useBookingContext();
+  const { setPageName, setCleaningData, cleaningData } = useBookingContext()
 
-  const handleClick = (e: any): void => {
-    setPageName("personalInformation");
-    e.preventDefault();
-  };
+  const handleClick = (e: React.MouseEvent<HTMLElement>): void => {
+    setPageName('personalInformation')
+    e.preventDefault()
+  }
 
   return (
     <div>
-      <h3 className="py-4">Book Now and get same day response!</h3>
+      <h3 className='py-4'>Book Now and get same day response!</h3>
       <h3>1. Cleaning Information</h3>
       <Formik
         initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
+          bedroom: '',
+          lastName: '',
+          email: ''
         }}
         onSubmit={(
           values: Values,
           { setSubmitting }: FormikHelpers<Values>
         ) => {
           setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
+            alert(JSON.stringify(values, null, 2))
+            setSubmitting(false)
+          }, 500)
         }}
       >
         <Form>
           {/* <label htmlFor='firstName'>First Name</label> */}
-          <div className="flex flex-col max-w-lg py-4">
-            <div className="grid gap-4 grid-cols-2 grid-rows-2">
-              <FormCounter label={"Bedrooms"} />
+          <div className='flex flex-col max-w-lg py-4'>
+            <div className='grid gap-4 grid-cols-2 grid-rows-2'>
+              <FormCounter label={'Bedrooms'} />
 
-              <FormCounter label={"Bathrooms"} />
+              <FormCounter label={'Bathrooms'} />
 
-              <FormCounter label={"Livingrooms"} />
+              <FormCounter label={'Livingrooms'} />
 
-              <FormCounter label={"Kitchens"} />
+              <FormCounter label={'Kitchens'} />
             </div>
 
-            <div className="flex justify-between py-2">
-              <div className="mr-1 w-full">
+            <div className='flex justify-between py-2'>
+              <div className='mr-1 w-full'>
                 <Field
-                  id="date"
-                  name="date"
-                  placeholder="02/13/2022"
+                  id='date'
+                  name='date'
+                  placeholder='02/13/2022'
                   as={CustomInputComponent}
                 />
               </div>
 
-              <div className="ml-1 w-full">
+              <div className='ml-1 w-full'>
                 <Field
-                  id="time"
-                  name="time"
-                  placeholder="2:00 PM"
+                  id='time'
+                  name='time'
+                  placeholder='2:00 PM'
                   as={CustomInputComponent}
                 />
               </div>
             </div>
-            <div className="grid gap-4 grid-cols-2 py-4 justify-items-start">
-              <Checkbox label="Clean Inside Fridge" price="40" />
-              <Checkbox label="Clean Inside Oven" price="40" />
-              <Checkbox label="Deep Clean" price="40" />
-              <Checkbox label="Laundry" price="40" />
-              <Checkbox label="Dishes" price="30" />
-              <Checkbox label="Assemble Furniture (1 hr)" price="60" />
+            <div className='grid gap-4 grid-cols-2 py-4 justify-items-start'>
+              <Checkbox label='Clean Inside Fridge' price='40' />
+              <Checkbox label='Clean Inside Oven' price='40' />
+              <Checkbox label='Deep Clean' price='40' />
+              <Checkbox label='Laundry' price='40' />
+              <Checkbox label='Dishes' price='30' />
+              <Checkbox label='Assemble Furniture (1 hr)' price='60' />
             </div>
           </div>
           <PriceQuoteLabel />
           <Button
-            type="submit"
-            className="flex flex-row-reverse py-4"
+            type='submit'
+            className='flex flex-row-reverse py-4'
             onClick={(e) => handleClick(e)}
           >
             Next
@@ -92,7 +92,7 @@ const CleaningInformationForm = (): JSX.Element => {
         </Form>
       </Formik>
     </div>
-  );
-};
+  )
+}
 
-export default CleaningInformationForm;
+export default CleaningInformationForm
--- src/domains/booking-experience/context/BookingContext.tsx
+++ src/domains/booking-experience/context/BookingContext.tsx
@@ -1,73 +1,76 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from 'react'
+
+interface IExtras {
+  fridge: boolean
+  oven: boolean
+  deepClean: boolean
+  laundry: boolean
+  dishes: boolean
+  furniture: boolean
+}
 
 interface ICleaningData {
-  bedrooms: number;
-  bathrooms: number;
-  livingrooms: number;
-  kitchens: number;
-  date: string;
-  time: string;
-  extras: {
-    fridge: boolean;
-    oven: boolean;
-    deepClean: boolean;
-    laundry: boolean;
-    dishes: boolean;
-    furniture: boolean;
-  };
+  [key: string]: number | string | IExtras
+  bedrooms: number
+  bathrooms: number
+  livingrooms: number
+  kitchens: number
+  date: string
+  time: string
+  extras: IExtras
 }
 
 interface PageNameContextType {
-  pageName: string;
-  setPageName: (pageName: string) => void;
-  cleaningData: ICleaningData;
-  setCleaningData: (cleaningData: ICleaningData) => void;
+  pageName: string
+  setPageName: (pageName: string) => void
+  cleaningData: ICleaningData
+  setCleaningData: (cleaningData: ICleaningData) => void
 }
 
 interface IProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 const BookingContext = createContext<PageNameContextType>({
-  pageName: "",
+  pageName: '',
   setPageName: () => {},
   cleaningData: {
     bedrooms: 0,
     bathrooms: 0,
     livingrooms: 0,
     kitchens: 0,
-    date: "",
-    time: "",
+    date: '',
+    time: '',
     extras: {
       fridge: false,
       oven: false,
       deepClean: false,
       laundry: false,
       dishes: false,
-      furniture: false,
-    },
+      furniture: false
+    }
   },
-  setCleaningData: () => {},
-});
+  setCleaningData: () => {}
+})
 
 const BookingProvider = ({ children }: IProps): JSX.Element => {
-  const [pageName, setPageName] = useState("cleaningInformation");
+  const [pageName, setPageName] = useState('cleaningInformation')
   const [cleaningData, setCleaningData] = useState({
     bedrooms: 0,
     bathrooms: 0,
     livingrooms: 0,
     kitchens: 0,
-    date: "",
-    time: "",
+    date: '',
+    time: '',
     extras: {
       fridge: false,
       oven: false,
       deepClean: false,
       laundry: false,
       dishes: false,
-      furniture: false,
-    },
-  });
+      furniture: false
+    }
+  })
 
   return (
     <BookingContext.Provider
@@ -75,9 +78,9 @@ const BookingProvider = ({ children }: IProps): JSX.Element => {
     >
       {children}
     </BookingContext.Provider>
-  );
-};
+  )
+}
 
-const useBookingContext = () => useContext(BookingContext);
+const useBookingContext = () => useContext(BookingContext)
 
-export { BookingProvider, useBookingContext };
+export { BookingProvider, useBookingContext }
